6d647b384022d44f209a81812730dec55b28ae10
mohanxz <mohanrajs2k04@gmail.com>
2026-05-01 11:18:43 +0530

    loginservices _env check

--- loginserver/backend/server.js
+++ loginserver/backend/server.js
@@ -37,5 +37,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/auth', authRoutes);
 
+const PORT = process.env.PORT || 5004;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+module.exports = app;
