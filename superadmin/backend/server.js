721f3d8837753555848cbaeee474c28e96eb3f7f
mohanxz <mohanrajs2k04@gmail.com>
2026-05-01 14:43:45 +0530

    feat: initialize backend Express server with database connection and API routing configuration

--- superadmin/backend/server.js
+++ superadmin/backend/server.js
@@ -28,6 +28,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'https://cybernaut-lms-v2.onrender.com',
+  "https://cybernaut-indol.vercel.app"
 ];
 
 app.use(cors({
